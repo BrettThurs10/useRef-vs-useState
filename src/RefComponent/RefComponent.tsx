9a55f47c9481a4401b0c25cbdf7e6b040262eee9
Brett Thurston <bthurston@babelstreet.com>
2022-05-25 09:00:19 -0500

    add button element. add times rendered ref. add comments

--- src/RefComponent/RefComponent.tsx
+++ src/RefComponent/RefComponent.tsx
@@ -1,44 +1,65 @@
 import { useRef, useState, ChangeEvent, useEffect } from "react";
 import { styles } from "./RefComponentStyles";
 const RefComponent = () => {
-  const inputRef = useRef("🥧");
+  // we are making an abstract reference with dataref since it's not attached to a the "ref" attribute
+  const dataRef = useRef("🥧");
+  // we are making a direct reference to the 2nd input element below by attaching this const to it's "ref" attribute -> see below in render
+  const inputRef = useRef<HTMLInputElement>(null);
+  // we are making another abstract reference here to hold the count of how many times this component has rendered
+  const timesRendered = useRef(0);
+  // we are making a state variable here and assigning it's value to the 2nd input below. by doing this we will cause a re-render whenever we set it's value
   const [inputString, setInputString] = useState("🍕");
-  let [timesRendered, setTimesRendered] = useState(0);
 
-  const updateRef = (e: ChangeEvent<HTMLInputElement>) => {
-    inputRef.current = e.target.value;
-    console.log(inputRef.current);
-    //look at your console and notice how the component is NOT rendering when you type into the useRef input
+  // this function will update the dataRef const. notice how we are updating the "current" property on dataRef. also, notice how in console the component is NOT rendering when you type into the dataRef input
+  const updateDataRef = (e: ChangeEvent<HTMLInputElement>) => {
+    dataRef.current = e.target.value;
+    console.log(dataRef.current);
   };
+
+  // here we are making a method to update our state variable and assigning it to the "onChange" attribute on the inputString input
   const handleOnChange = (e: ChangeEvent<HTMLInputElement>) => {
     setInputString(e.target.value);
   };
+  // this method just logs the timesRendered and which useEffect block the render happened from
   const renderMsg = (fromWhere: string) => {
-    console.log("✨ Component rendered because of " + fromWhere);
+    console.log(
+      `✨ Component has rendered ${timesRendered.current} times and most recently from ${fromWhere}`
+    );
   };
+
+  // a helper method to help with logging
   const whereFromMsg = (type: string, value: string) => {
     console.log(`${type} === ${value}`);
   };
 
-  const updateTimesRendered = () => setTimesRendered(timesRendered + 1);
-  const renderTimesRendered = () => timesRendered;
+  // a method to update our timesRendered ref
+  const updateTimesRendered = () =>
+    (timesRendered.current = timesRendered.current + 1);
 
+  // method to focus the inputRef input element
+  const handleOnClick = () => {
+    inputRef?.current?.focus();
+  };
+
+  // let's log the first time this component actually renders. dev note: removing  <React.StrictMode> that wraps your <App /> by default with Create React app will make React components only render once. With strict mode enable they will render twice as of React 18.
   useEffect(() => {
     updateTimesRendered();
   }, []);
 
+  // Whenever the dataRef is changed, let's log it
   useEffect(() => {
     updateTimesRendered();
-    renderMsg("inputRef useEffect");
-    whereFromMsg("inputRef", inputRef.current);
-  }, [inputRef]);
+    renderMsg("dataRef useEffect");
+    whereFromMsg("dataRef", dataRef.current);
+  }, [dataRef]);
 
+  // Whenever the inputString is changed, let's log it
   useEffect(() => {
     updateTimesRendered();
     renderMsg("inputString useEffect");
     whereFromMsg("inputString", inputString);
     // uncomment to see how useRef can capture the previous state, but not current. i.e. typing in dog in the useState input you will see 'dog' and in the useRef value you will see 'do'
-    // inputRef.current = inputString;
+    // dataRef.current = inputString;
   }, [inputString]);
 
   return (
@@ -48,22 +69,25 @@ const RefComponent = () => {
       <div style={{ width: "300px", alignSelf: "center" }}>
         <p>
           Sometimes you want to capture data without causing a render of a
-          component or components. useRef is a workaround for that. Check out
-          the console in your browser via Inspect Element to see what's
-          happening and when.
+          component or components. Utilizing the useRef hook is a workaround for
+          that. Check out the console in your browser via Inspect Element to see
+          what's happening and when.
         </p>
-        <p style={styles.label}>useRef value:</p>
-        <pre>{inputRef.current}</pre>
+
+        <p style={styles.label}>dataRef.current value:</p>
+        <pre>{dataRef.current}</pre>
         <input
           placeholder="type something"
-          onChange={updateRef}
+          onChange={updateDataRef}
           type="text"
           id="fname"
           name="fname"
         />
-        <p style={styles.label}>useState value:</p>
+
+        <p style={styles.label}>inputString value:</p>
         <pre>{inputString}</pre>
         <input
+          ref={inputRef}
           placeholder="type something"
           onChange={handleOnChange}
           type="text"
@@ -71,10 +95,9 @@ const RefComponent = () => {
           name="fname"
         />
 
-        <p style={styles.label}>component has rendered:</p>
-        <pre>{`${renderTimesRendered()} ${
-          timesRendered > 1 ? "times" : "time"
-        }`}</pre>
+        <div style={{ paddingBottom: "20px" }} />
+
+        <button onClick={handleOnClick}>Focus inputRef</button>
       </div>
     </div>
   );
